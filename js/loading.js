744203c0bee85da901ee080172da2164f9be48c1
“Yanwei <miaoywstyle@msn.com>
2022-10-14 20:47:55 +0200

    Remove loading animation

--- js/loading.js
+++ js/loading.js
@@ -1,11 +1,12 @@
-/* LOADING */
+/* LOADING 
 var myVar;
 
 function myFunction() {
-  myVar = setTimeout(showPage, 4000);
+  myVar = setTimeout(showPage, 1000);
 }
 
 function showPage() {
   document.getElementById("loader").style.display = "none"; 
   document.getElementById("timeOut").style.visibility = "visible"; // show page after the loading animation
 }
+*/
