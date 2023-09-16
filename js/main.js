7978d0145d42ae4495a2b5235313dbc5b9dcb5da
“Yanwei <miaoywstyle@msn.com>
2023-09-16 20:08:43 +0200

    update to my website version 3.0

--- js/main.js
+++ js/main.js
@@ -3,12 +3,10 @@ $.get("/html/navbar.html", function(data){
     $("#nav-placeholder").replaceWith(data);
 });
 
-
 // Show Footer
 $.get("/html/footer.html", function(data){
     $("#footer-placeholder").replaceWith(data);
 });
-
 
 // Google Analytics
 window.dataLayer = window.dataLayer || [];
@@ -16,12 +14,3 @@ function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 
 gtag('config', 'UA-244951959-1');
-
-
-// Animation
-var typed = new Typed(".type", {
-    strings: ["do research", "offer councel", "design interfaces"],
-    typeSpeed: 150,
-    backSpeed: 150,
-    loop: true
-})
