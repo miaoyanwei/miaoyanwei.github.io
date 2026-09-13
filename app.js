89c910bf0655d79b7528880edcf1e64e659562e8
Yanwei Miao <miaoywstyle@msn.com>
2026-09-13 12:11:23 +0200

    Update qna

--- app.js
+++ app.js
@@ -486,6 +486,6 @@ chatScroll.addEventListener("click", (e)=>{
 
 /* ---------- boot ---------- */
 addBotMessage(
-  "Hi, I'm Miao (not really), a user experience designer. Ask anything about me, my work, and my design philosophy!"
+  "Hi, I'm Miao (not really), a User Experience Designer. Ask anything about me, my work, and my design philosophy!"
 );
 addQuickStartChips();
